852e9071b905b5027ab6f9f2b3244d6bf539afe8
Sławek <slawek@MacBook-Air-Patrycja.local>
2023-09-12 19:34:44 +0200

    spy

--- monte_carlo/dice.py
+++ monte_carlo/dice.py
@@ -6,3 +6,13 @@ Created on Tue Sep 12 16:28:44 2023
 @author: slawek
 """
 
+import random
+
+def random_numbers():
+    return random.random()
+
+root=10
+number=random_numbers()
+print(number)
+
+print(2**2)
